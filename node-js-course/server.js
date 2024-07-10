696711e0ca3b93e883a1e266361531e9258f397a
jantupas <janedgartupas@gmail.com>
2024-07-10 17:41:07 +0800

    second commit

--- node-js-course/server.js
+++ node-js-course/server.js
@@ -6,8 +6,7 @@ const server = http.createServer((req, res) => {
     // res.statusCode = 404;
     // res.write('Hello World!');
 
-    res.writeHead(500, { 'Content-Type': 'application/json'});
-    res.end(JSON.stringify({ message: 'Server Error' }))
+    res.writeHead(200, { 'Content-Type': 'text/html'});
     res.end('<h1>Hello World!</h1>');
  });
 
